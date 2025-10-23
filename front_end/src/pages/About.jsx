768144bb3b95a29bea54d32ff0f4e1c03d22fcd9
Berita-Winnie <beritawinnie@gmail.com>
2025-10-23 12:23:17 +0300

    feat(contact): complete design and layout for contact us page

--- front_end/src/pages/About.jsx
+++ front_end/src/pages/About.jsx
@@ -6,35 +6,34 @@ import NewsLetterBox from '../components/NewsLetterBox'
 const About = () => {
   return (
     <div>
-      <div className="text-2xl text-center pt-8  border-t">
-        <Title text1={'ABOUT'} text2={'US'} />
-      </div>
-      <div className="my-10 flex flex-col md:flex-row gap-16">
-        <img
-          className="w-full md:max-w-[450px] "
-          src={assets.about_img}
-          alt=""
-        />
-        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-            iure non nemo dicta officiis blanditiis, quo, et aliquam voluptatem
-            iusto reprehenderit magnam ducimus rem, voluptatum quod dolorum nam
-            alias dolores!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, amet
-            aperiam adipisci sint officiis accusamus magni inventore vel atque
-            expedita et cumque nihil. Ab, quo quam fugiat molestias perspiciatis
-            nihil!
-          </p>
-          <p className="text-gray-800 ">Our Mission</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
-            dolorum voluptates natus repellendus iure animi, quasi eveniet
-            facilis eos adipisci ab officia soluta placeat eius ad sequi sint!
-            Possimus, nam.
-          </p>
+      <div>
+        <div className="text-2xl text-center pt-8  border-t">
+          <Title text1={'ABOUT'} text2={'US'} />
+        </div>
+        <div className="my-10 flex flex-col md:flex-row gap-16">
+          <img
+            className="w-full md:max-w-[450px] "
+            src={assets.about_img}
+            alt=""
+          />
+          <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              iure non nemo dicta officiis blanditiis, quo, et aliquam
+              voluptatem
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
+              amet aperiam adipisci sint officiis accusamus magni inventore vel
+              atque
+            </p>
+            <p className="text-gray-800 ">Our Mission</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
+              dolorum voluptates natus repellendus iure animi, quasi eveniet
+              facilis eos
+            </p>
+          </div>
         </div>
         <div className="text-xl py-4 ">
           <Title text1={'WHY'} text2={'CHOOSE US'} />
